ab5f3c7ae1da26bafb037d721b5ca0bc95e648c3
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-08-08 11:17:29 +0300

    fix(task2):ignore capitalization and space

--- Task-two/Word_frequency_count.go
+++ Task-two/Word_frequency_count.go
@@ -1,15 +1,39 @@
 package main
+
 import (
-    "strings"
+	"strings"
+	"unicode"
 )
 
-func WordFrequencyCount(text string) map[string]int {
-    words := strings.Fields(text)
-    wordFrequency := map[string]int{}
-    for _, word := range words {
-        wordFrequency[word]++
-    }
 
-    return wordFrequency
+// Function to count the frequency of each word in a string
+func wordFrequency(s string) map[string]int {
+	// Create a map to store the frequency of each word
+	frequencyMap := make(map[string]int)
+
+	// Convert the string to lowercase
+	s = strings.ToLower(s)
+
+	// Function to remove punctuation from a string
+	removePunctuation := func(r rune) rune {
+		if unicode.IsPunct(r) {
+			return -1
+		}
+		return r
+	}
+
+	// Remove punctuation from the string
+	s = strings.Map(removePunctuation, s)
+
+	// Split the string into words
+	words := strings.Fields(s)
+
+	// Count the frequency of each word
+	for _, word := range words {
+		frequencyMap[word]++
+	}
+
+	return frequencyMap
 }
+
 
